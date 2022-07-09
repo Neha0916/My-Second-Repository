b6d643f9723d51402ddc2a4c093f8facc0b35a75
Neha Kumari <cutiepieneha789@gmail.com>
2022-07-10 01:52:57 +0530

    beginning with javascript

---
+++ experiment.js
@@ -0,0 +1,13 @@
+const monthlyRent = 500;
+
+const yearlyRent = monthlyRent * 12;
+console.log(yearlyRent);
+
+const myName="Neha";
+const lastName="Kumari";
+console.log(myName);
+
+const sentence="Hello "+myName+" "+lastName+"! How are you!!?";
+const sentenceWithTemplate=`Hello ${myName} ${lastName}! How are you!!?`;
+console.log(sentence);
+console.log(sentenceWithTemplate);
